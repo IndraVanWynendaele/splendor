c67e4f20988c87196e55170706ea57a26c240095
Youna Noynaert <youna.noynaert@student.hogent.be>
2023-03-15 23:34:23 +0100

    aanpassing controleerGebruikersnaam

--- src/domein/Speler.java
+++ src/domein/Speler.java
@@ -25,13 +25,19 @@ public class Speler {
 	
 	private void controleerGebruikersnaam(String gebruikersnaam) {
 		String specialeKarakters = "!#$%&'()*+,-./:;<=>?@[]^`{|}";
+		boolean goed;
 		for(int i = 0; i < specialeKarakters.length(); i++)
 			if(gebruikersnaam.contains(Character.toString(specialeKarakters.charAt(i))))
 				throw new IllegalArgumentException("gebruikersnaam mag geen speicale tekens buiten spatie en _ bevatten");
-		
 		char c = gebruikersnaam.charAt(0);
-		if((c < 'A' && c > 'Z') || (c < 'a' && c > 'z'))
+		/*if((c < 'A' && c > 'Z') || (c < 'a' && c > 'z'))
+			throw new IllegalArgumentException("Gebruikersnaam moet starten met een letter (klein of groot)!");*/
+		if((c >= 'A' && c <='Z') || (c >= 'a' && c <='z'))
+			goed = true;
+		else
 			throw new IllegalArgumentException("Gebruikersnaam moet starten met een letter (klein of groot)!");
+
+		
 	}
 	
 	private void controleerGeboortejaar(LocalDate geboortejaar) {
