d12ddd0ebfd07f3b6d29c5649bcf5c1218734585
Indra Van Wynendaele <indra.vanwynendaele@student.hogent.be>
2023-03-15 17:48:02 +0100

    implementatie klasse speler

--- src/domein/Speler.java
+++ src/domein/Speler.java
@@ -7,6 +7,7 @@ public class Speler {
 	private Spel s;
 	private final String gebruikersnaam;
 	private final LocalDate geboortejaar;
+	private final LocalDate huidigJaar = LocalDate.parse("2017");
 
 	public String getGebruikersnaam() {
 		return this.gebruikersnaam;
@@ -16,14 +17,24 @@ public class Speler {
 		return this.geboortejaar;
 	}
 
-	/**
-	 * 
-	 * @param gebruikersnaam
-	 * @param geboortejaar
-	 */
 	public Speler(String gebruikersnaam, LocalDate geboortejaar) {
-		// TODO - implement Speler.Speler
-		throw new UnsupportedOperationException();
+		controleerGebruikersnaam(gebruikersnaam);
+		controleerGeboortejaar(geboortejaar);
+		this.gebruikersnaam = gebruikersnaam;
+		this.geboortejaar = geboortejaar;
 	}
-
+	
+	private void controleerGebruikersnaam(String gebruikersnaam) {
+		if(!gebruikersnaam.contains(" ") || !gebruikersnaam.contains("_"))
+			throw new IllegalArgumentException("Gebruikersnaam mag enkel spaties en _ bevatten, anders geen speciale tekens!");
+		char c = gebruikersnaam.charAt(0);
+		if((c <= 'A' && c >= 'Z') || (c <= 'a' || c >= 'z'))
+			throw new IllegalArgumentException("Gebruikersnaam moet starten met een letter (klein of groot)!");
+	}
+	
+	private void controleerGeboortejaar(LocalDate geboortejaar) {
+		if(geboortejaar.isAfter(huidigJaar)) {
+			throw new IllegalArgumentException("Speler moet minimum 6 jaar oud zijn");
+		}
+	}
 }
