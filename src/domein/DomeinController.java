841dfa5ce778433299146ffe1061ddc74b897d9c
Anke Janssens <anke.janssens@student.hogent.be>
2023-03-15 21:56:02 +0100

    Deel 2 domeincontroller

--- src/domein/DomeinController.java
+++ src/domein/DomeinController.java
@@ -4,14 +4,60 @@ package domein;
 import java.time.LocalDate;
 import java.util.*;
 
+import javafx.scene.control.Spinner;
+
 public class DomeinController {
 
 	private List<Speler> spelers;
 	private SpelerRepository sRepo;
 	private Spel s;
+	
+	private Scanner input = new Scanner(System.in);
+	private boolean gebruikerGedaan;
 
 	public void startSpel() {
-		spelers=sRepo.getSpelers();
+		String naam;
+		String stringJaar;
+		LocalDate jaar;
+		
+		System.out.print("Geef de gebruikersnaam: ");
+		naam= input.nextLine();
+		System.out.print("Geef je geboortjejaar: ");
+		stringJaar=input.nextLine();
+		
+		jaar = LocalDate.parse(stringJaar);
+		
+		meldAan(naam,jaar);
+		
+		//spelers=sRepo.getSpelers();
+		//Speler startSpeler=spelers.get(0);
+		//spelers.remove(0);
+		
+		int i=1;
+		do {
+			i++;
+			System.out.printf("Geef de gebruikersnaam van speler %d: ",i+1);
+			naam= input.nextLine();
+			System.out.printf("Geef je geboortjejaar van speler %d: ",i+1);
+			stringJaar=input.nextLine();
+			
+			jaar = LocalDate.parse(stringJaar);
+				
+			meldAan(naam,jaar);
+				
+			System.out.printf("Wil je nog een %d de speler toevoegen? (1=ja, 2=nee)");
+			int keuze = input.nextInt();
+			switch(keuze) {
+			case 1 -> gebruikerGedaan=false;
+			case 2 -> gebruikerGedaan=true;
+			default -> throw new IllegalArgumentException("Je moet 1 of 2 ingeven");
+			}
+			if (i==4) {
+				 gebruikerGedaan=true;
+			}
+		}while(!gebruikerGedaan);
+		
+		
 	}
 
 	/**
@@ -23,7 +69,7 @@ public class DomeinController {
 		s = new Spel();
 		Speler s = new Speler(gebruikersnaam,geboortejaar);
 		sRepo.voegToe(s);
-		
+		spelers.add(s);
 		//Speler startSpeler = new Speler(gebruikersnaam,geboortejaar);
 		//List<Speler> spelers= new ArrayList<>();
 	}
@@ -33,6 +79,9 @@ public class DomeinController {
 		sRepo = new SpelerRepository();
 		
 	}
-
+// ik snap niet goed hoe ik de link moet leggen tussen speler en spel en hoe ik dan 
+// aanduid welke speler de startspeler is en welke de oovergebleve speles zijn
+// ook snap ik niet zo goed of ik de aangemaakte speler moet toevoegen aan de repo 
+// of aan het aangemaakte attribut die in deze klasse staat
 
 }
