c22c539f4ede2866841dc4e65afd9f2e3daf299d
Anke Janssens <anke.janssens@student.hogent.be>
2023-03-16 14:35:43 +0100

    Mapper

--- src/persistentie/SpelerMapper.java
+++ src/persistentie/SpelerMapper.java
@@ -2,6 +2,7 @@ package persistentie;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import domein.Speler;
@@ -9,13 +10,15 @@ import domein.Speler;
 public class SpelerMapper {
 	
 	public SpelerMapper() {
+		
+	}
+		
+	public Speler geefSpeler(String gebruikersnaam,int geboortejaar) {
 		try(Connection conn = DriverManager.getConnection(Connectie.JDBC_URL)){
-			
+			 PreparedStatement query = conn.prepareStatement("SELECT * FROM ID399.Speler");
 		}catch(SQLException exception) {
 			throw new RuntimeException(exception);
 		}
-	}
-	public Speler geefSpeler(String gebruikersnaam,int geboortejaar) {
-		
+	
 	}
 }
