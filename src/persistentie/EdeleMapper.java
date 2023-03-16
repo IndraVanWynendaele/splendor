ccc76124cbcff85e06b5d1938798241809e1c47a
Youna Noynaert <youna.noynaert@student.hogent.be>
2023-03-16 15:43:30 +0100

    aanmaak edelerepo

--- src/persistentie/EdeleMapper.java
+++ src/persistentie/EdeleMapper.java
@@ -1,10 +1,5 @@
 package persistentie;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 public class EdeleMapper {
-	try(Connection conn = DriverManager.getConnection(Connectie.JDBC_URL)){
-		
-	}
+	
 }
