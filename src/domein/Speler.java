869ebca9fdbbf8b0a772848b31706a081f200c8a
Youna Noynaert <youna.noynaert@student.hogent.be>
2023-03-16 15:35:23 +0100

    aanpassing speler

--- src/domein/Speler.java
+++ src/domein/Speler.java
@@ -1,13 +1,11 @@
 package domein;
 
-import java.time.LocalDate;
-
 public class Speler {
 
-	private Spel s;
 	private final String gebruikersnaam;
 	private final int geboortejaar;
-	private final int minimumJaar = 2017;
+	private final int HUIDIG_JAAR = 2023;
+	private final int MINIMUM_JAAR = HUIDIG_JAAR - 6;
 
 	public String getGebruikersnaam() {
 		return this.gebruikersnaam;
@@ -28,14 +26,14 @@ public class Speler {
 		String specialeKarakters = "!#$%&'()*+,-./:;<=>?@[]^`{|}";
 		for(int i = 0; i < specialeKarakters.length(); i++)
 			if(gebruikersnaam.contains(Character.toString(specialeKarakters.charAt(i))))
-				throw new IllegalArgumentException("gebruikersnaam mag geen speicale tekens buiten spatie en _ bevatten");
+				throw new IllegalArgumentException("gebruikersnaam mag geen speciale tekens buiten spatie en _ bevatten");
 		char c = gebruikersnaam.charAt(0);
 		if(!((c >= 'A' && c <='Z') || (c >= 'a' && c <='z')))
 			throw new IllegalArgumentException("Gebruikersnaam moet starten met een letter (klein of groot)!");		
 	}
 	
 	private void controleerGeboortejaar(int geboortejaar) {
-		if(geboortejaar > minimumJaar)
+		if(geboortejaar > MINIMUM_JAAR)
 			throw new IllegalArgumentException("Gebruiker moet minimum 6 jaar oud zijn");
 	}
 }
