26bd38dc7ec2e94d0ae676a8384f9b120f96b3b8
Anke Janssens <anke.janssens@student.hogent.be>
2023-03-17 10:54:43 +0100

    Databank sql connecti

--- src/persistentie/Connectie.java
+++ src/persistentie/Connectie.java
@@ -1,5 +1,5 @@
 package persistentie;
 
 public class Connectie {
-	public static final String JDBC_URL = "jdbc:mysql://ID399796_g050.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID399796_g050&password=7cE77JrZ%8#6";
+	public static final String JDBC_URL = "jdbc:mysql://ID399796_g050.db.webhosting.be?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID399796_g050&password=7cE77JrZ%258#6";
 }
