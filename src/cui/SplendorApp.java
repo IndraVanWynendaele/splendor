690798efa224484b5027b6e97f0c8fa3ab2e7ce2
Youna Noynaert <youna.noynaert@student.hogent.be>
2023-03-20 18:56:59 +0100

    PrutsCommit #2

--- src/cui/SplendorApp.java
+++ src/cui/SplendorApp.java
@@ -22,16 +22,11 @@ public class SplendorApp {
 	private int geboortejaar;
 	
 	public SplendorApp(DomeinController dc) {
-		this.dc= dc;
+		this.dc = dc;
 	}
 	public void start() {		
-		System.out.println("Maak je keuze: ");
+		System.out.println("Maak je keuze (1-3): ");
 		keuzeKeuzeMenu = toonKeuzeMenu();
-		
-		
-		
-		
-		
 	}
 	
 	private int toonKeuzeMenu() {
@@ -43,79 +38,10 @@ public class SplendorApp {
 				keuzeKeuzeMenu = input.nextInt();
 			}catch(InputMismatchException exc) {
 				System.out.println("Keuze moet een nummer zijn!");
+			}finally {
+				input.nextLine();
 			}
-		}while(keuzeKeuzeMenu < 1 && keuzeKeuzeMenu > 3);
+		}while(keuzeKeuzeMenu < 1 || keuzeKeuzeMenu > 3);
 		return keuzeKeuzeMenu;
-	}
-	
-	/*public String toonSpelers() {
-		dbSpelers = dc.geefSpelers();
-		String spelerLijst = "";
-		int index = 1;
-		System.out.printf("%15s %15s%15s", "Speler", "Gebruikersnaam", "Geboortejaar");
-		for(Speler s: dbSpelers) 
-			spelerLijst += String.format("%15d:%15s%15d",index++, s.getGebruikersnaam(), s.getGeboortejaar());
-		return spelerLijst;		
-	}
-	
-	public int maakKeuze() {
-		dbSpelers = dc.geefSpelers();
-		do {
-			System.out.println("Geef het nummer van de speler waarmee je wil spelen: ");
-			keuze = input.nextInt();
-		}while(keuze > dbSpelers.size() + 1);
-		return keuze;
-	}*/
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*
-		 * String naam;
-		int jaar;
-		
-		System.out.print("Geef de gebruikersnaam: ");
-		naam= input.nextLine();
-		System.out.print("Geef je geboortjejaar (formaat: jjjj): ");
-		jaar=input.nextInt();
-		
-		meldAan(naam,jaar);
-		
-		//List<Spelers>
-		//spelers=sRepo.getSpelers();
-		//Speler startSpeler=spelers.get(0);
-		//spelers.remove(0);
-		
-		int i=1;
-		do {
-			i++;
-			System.out.printf("Geef de gebruikersnaam van speler %d: ",i+1);
-			naam= input.nextLine();
-			System.out.printf("Geef je geboortjejaar van speler %d (formaat: jjjj): ",i+1);
-			jaar=input.nextInt();
-				
-			meldAan(naam,jaar);
-				
-			System.out.printf("Wil je nog een %d de speler toevoegen? (1=ja, 2=nee)");
-			int keuze = input.nextInt();
-			switch(keuze) {
-			case 1 -> gebruikerGedaan=false;
-			case 2 -> gebruikerGedaan=true;
-			default -> throw new IllegalArgumentException("Je moet 1 of 2 ingeven");
-			}
-			if (i==4) {
-				 gebruikerGedaan=true;
-			}
-		}while(!gebruikerGedaan);
-		 * */
-	
+	}	
 }
